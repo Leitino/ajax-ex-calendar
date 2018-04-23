706e279639e7035950e52c785571d65fd0f1e950
Leitino <alessandro.leita89@gmail.com>
2018-04-23 16:34:34 +0200

    primo punto rifatto da zero

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 $(function() {
-  $('#button').click(function() {
+  var mese = '01'
+  var country = 'IT'
+  generateDays()
 
-    var country = $('#nazione').val()
-    console.log(country)
+  $('#next').click(function() {
+    mese = parseInt(mese) + 1
+    generateDays()
+  });
+  $('#prev').click(function() {
+    mese = parseInt(mese) - 1
+    generateDays()
+  });
 
+  function generateDays() {
+    var date = moment('2017-' + mese + '-01')
+    var giorni = date.daysInMonth() //quanti giorni in un mese
+    var meseinlettere = date.format('MMMM') //mese in lettere
+    $('.container').html('')
+    for (var i = 1; i <= giorni; i++) {
+      var day = moment('2017-' + mese + '-' + i)
+      console.log(day)
+      $('.container').append('<div class ="date-item" dateymd="'+day.format('YYYY-MM-DD')+'">'+ i + meseinlettere + '</div>')
+    }
     $.ajax({
       url:"https://holidayapi.com/v1/holidays",
       method:"GET",
@@ -11,21 +29,18 @@ $(function() {
         key:'54a6fac8-a41c-4f07-9392-cac57a3b19e9',
         country: country,
         year: 2017,
-        month: 01,
+        month: mese,
       },
       success:function(data) {
-        console.log(data)
+        for (var i = 0; i < data.holidays.length; i++) {
+          var festaymd = data.holidays[i].date;
+          $('.date-item').each(function() {
+            if ($(this).attr('dateymd') == festaymd)
+            $(this).addClass('red')
 
-        var daysInMonth = moment('2017-01').daysInMonth()
-        console.log(daysInMonth)
-        $('.risultati_giorni').html('')
-        for (var i = 1; i <= daysInMonth; i++) {
-          $('.risultati_giorni').append('<li> '+ i + '</li>')
+          })
         }
-
-      }
-    });
-
-  })
-
-})
+     }
+   })
+  }
+});
